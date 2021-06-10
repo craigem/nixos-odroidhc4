6ef02d34bb02218222c9594d778116ea91eedb74
Craige McWhirter <craige@mcwhirter.io>
2021-06-10 21:11:07 +1000

    ssh: updated authorized_keys

--- modules/odroidhc4/default.nix
+++ modules/odroidhc4/default.nix
@@ -1,9 +1,6 @@
 { pkgs, config, lib, modulesPath, ... }:
-with lib;
-{
-  imports = [
-    ../uboot/hardkernel-uboot.nix
-  ];
+with lib; {
+  imports = [ ../uboot/hardkernel-uboot.nix ];
   # The linux kernel used is compiled from the Hardkernel fork of
   # torvalds/linux
   boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_hardkernel;
@@ -12,7 +9,8 @@ with lib;
 
   # Remove zfs from supported filesystems as it fails due to not being able to
   # build the kernel module
-  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  boot.supportedFilesystems =
+    lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 
   # We do know the hardware we are planning to deploy to
   hardware.enableRedistributableFirmware = mkForce false;
@@ -26,6 +24,22 @@ with lib;
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = false;
   boot.loader.hardkernel-uboot.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+
+  # Add public SSH key to root user's authorized_keys file
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtjE0YstRzlh+Zhlj03th9DYOkMqJ5xHUcderBq151K craige@mcwhirter.io"
+  ];
+
+  # DNS
+  services.resolved.enable = true;
+  services.resolved.dnssec = "false";
+
+  # set a default root password
+  users.users.root.initialPassword = "toor";
 
   fileSystems = {
     "/boot" = {
